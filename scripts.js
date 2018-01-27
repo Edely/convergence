e0bf01faab6166759d5a4c0da300b35139447a3b
edelygomes <edelygomes@gmail.com>
2018-01-27 20:11:09 -0300

    linking data

--- scripts.js
+++ scripts.js
@@ -3,5 +3,36 @@ $(function(){
 
   $('#cidades .dropdown-item').click(function(){
     $('#menuCidades').text(this.text);
+    changeCity(this);
   });
+
+  var changeCity = function(node){
+    console.log($(node).attr('id'));
+  }
+
+  var data =
+    {
+      'sal':
+      {
+        'qua': {'max': 25, 'min': 10},
+        'qua': {'max': 25, 'min': 10},
+        'qui': {'max': 30, 'min': 25},
+        'qui': {'max': 30, 'min': 25},
+      },
+      'sp':
+      {
+        'qua': {'max': 25, 'min': 10},
+        'qua': {'max': 25, 'min': 10},
+        'qui': {'max': 30, 'min': 25},
+        'qui': {'max': 30, 'min': 25},
+      },
+      'rio':
+      {
+        'qua': {'max': 25, 'min': 10},
+        'qua': {'max': 25, 'min': 10},
+        'qui': {'max': 30, 'min': 25},
+        'qui': {'max': 30, 'min': 25},
+      },
+    };
+
 });
